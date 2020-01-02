738abd112a66a93908c4844dc8528ba14d259da0
Matt Plichta <bigcatplichta@gmail.com>
2020-01-02 20:42:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ function handleClick(e) {
 
 /* How would I handle negative number inputs? */
 function greet(time) {
-  const hour = time.split(":")[0]
-  if ( hour < 12 ) return "Good Morning"
-  if ( hour <= 17 ) return "Good Afternoon"
-  if ( hour > 17 ) return "Good Evening"
+  const hour = time.split(":")[0];
+  if ( hour < 12 ) return "Good Morning";
+  if ( hour <= 17 ) return "Good Afternoon";
+  if ( hour > 17 ) return "Good Evening";
 }
 
 /* Write your implementation of displayMessage() */
 
 function displayMessage(message) {
-  message = document.getElementById('greeting').innerText
+  document.getElementById('greeting').innerText = message;
 }
